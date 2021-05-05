70dc1dc2c711acc7898d359bcb1ac79a72746d2a
ejluciano <username@example.com>
2021-05-05 22:57:00 +0800

    I've managed to finish, understand and apply Python in 5 days

---
+++ Crash course/Classes/restaurant.py
@@ -0,0 +1,16 @@
+class restaurant:
+    def __init__(self, restaurant_name, cuisine_type):
+        self.restaurant_name = name
+        self.cuisine_type = type
+
+    def describe_restaurant(self):
+        print(f"The {self.name} is expensive. ")
+        print(f"The {type.name}'s food is really good though. ")
+
+    def open_restaurant(self):
+        print(f"The {self.name} is now open. ")
+
+restaurant_1 = restaurant("Aristocrat", "Bacon")
+print(f"The restaurant's name is {restaurant_1.name}")
+print(f"The food in {restaurant_1.type} is really good.")
+
