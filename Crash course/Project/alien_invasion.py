9c3ca698753bd17c6784fbde68a3a845c4a30664
ejluciano <username@example.com>
2021-05-05 23:09:07 +0800

    moving on to automating things with python

--- Crash course/Project/alien_invasion.py
+++ Crash course/Project/alien_invasion.py
@@ -1,14 +1,19 @@
 import sys
-
 import pygame
+from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     def __init__(self):
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200,800))
+        self.screen = pygame
+
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invation")
-        self.bg_color = (230, 230, 230)
+        self.ship = Ship(self)
+        
 
     def run_game(self):
         while True:
@@ -16,7 +21,8 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
             pygame.display.flip()
 
